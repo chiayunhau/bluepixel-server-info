3bb65236b345d000900cacc5665c579f7a265ad1
chiayunhau <yunhauchia@gmail.com>
2023-04-29 22:36:35 +0800

    cleaner code

--- server.py
+++ server.py
@@ -1,12 +1,7 @@
-import requests
 from flask import Flask, jsonify
+from fetch_cpu_usage import start_fetching
 
 app = Flask(__name__)
-
-# Replace with your own Pterodactyl API credentials and server ID
-API_URL = 'https://ctrl.cxmpute.com/api/client'
-API_KEY = 'ptlc_tTx1NlJbCmfVRXvzJu6O0t76E9j7kGnXZOrWruZRMmA'
-SERVER_ID = 'b3683f94-64d0-42ce-94ed-735fe30a8540'
 
 # Disable CORS in Flask
 @app.after_request
@@ -18,20 +13,7 @@ def add_header(response):
 
 @app.route('/server/cpu')
 def server_cpu():
-    headers = {
-        'Authorization': f'Bearer {API_KEY}',
-        'Accept': 'application/json'
-    }
-
-    # Make a GET request to the Pterodactyl API to retrieve server information
-    response = requests.get(f'{API_URL}/servers/{SERVER_ID}/resources', headers=headers)
-
-    if response.status_code == 200:
-        # Extract the CPU usage value from the response
-        cpu_usage = response.json()['attributes']['resources']['cpu_absolute']
-        return jsonify({'cpu_usage': cpu_usage})
-    else:
-        return jsonify({'error': 'Failed to retrieve server information'})
+    return jsonify({'cpu_usage': start_fetching(), 'cpu_limit': '200%'})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
