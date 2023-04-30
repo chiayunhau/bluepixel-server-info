131a4408347437bc23d0bfd7fb1d15a828fc7da9
chiayunhau <yunhauchia@gmail.com>
2023-04-30 10:50:05 +0800

    revamped

---
+++ fetch_usage.py
@@ -0,0 +1,66 @@
+import requests
+
+API_URL = 'https://ctrl.cxmpute.com/api/client'
+API_KEY = 'ptlc_tTx1NlJbCmfVRXvzJu6O0t76E9j7kGnXZOrWruZRMmA'
+SERVER_ID = 'b3683f94-64d0-42ce-94ed-735fe30a8540'
+
+# Function to fetch CPU usage data from the Pterodactyl API
+def fetch_cpu_usage():
+    headers = {
+        'Authorization': f'Bearer {API_KEY}',
+        'Accept': 'application/json'
+    }
+    response = requests.get(f'{API_URL}/servers/{SERVER_ID}/resources', headers=headers)
+    if response.status_code == 200:
+        data = response.json()['attributes']['resources']
+        cpu_usage = data['cpu_absolute']
+        return cpu_usage
+    else:
+        print('Failed to retrieve server information')
+        return None
+
+# Function to fetch RAM usage data from the Pterodactyl API
+def fetch_ram_usage():
+    headers = {
+        'Authorization': f'Bearer {API_KEY}',
+        'Accept': 'application/json'
+    }
+    response = requests.get(f'{API_URL}/servers/{SERVER_ID}/resources', headers=headers)
+    if response.status_code == 200:
+        data = response.json()['attributes']['resources']
+        ram_usage = data['memory_bytes'] / (1024 ** 3)
+        return ram_usage
+    else:
+        print('Failed to retrieve server information')
+        return None
+
+# Function to fetch disk usage data from the Pterodactyl API
+def fetch_disk_usage():
+    headers = {
+        'Authorization': f'Bearer {API_KEY}',
+        'Accept': 'application/json'
+    }
+    response = requests.get(f'{API_URL}/servers/{SERVER_ID}/resources', headers=headers)
+    if response.status_code == 200:
+        data = response.json()['attributes']['resources']
+        disk_usage = data['disk_bytes'] / (1024 ** 3)
+        return disk_usage
+    else:
+        print('Failed to retrieve server information')
+        return None
+
+# Function to fetch network usage data from the Pterodactyl API
+def fetch_network_usage():
+    headers = {
+        'Authorization': f'Bearer {API_KEY}',
+        'Accept': 'application/json'
+    }
+    response = requests.get(f'{API_URL}/servers/{SERVER_ID}/resources', headers=headers)
+    if response.status_code == 200:
+        data = response.json()['attributes']['resources']
+        network_rx = data['network_rx_bytes']
+        network_tx = data['network_tx_bytes']
+        return {'network_rx': network_rx, 'network_tx': network_tx}
+    else:
+        print('Failed to retrieve server information')
+        return None
--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify
-from fetch_cpu_usage import start_fetching
+from fetch_usage import fetch_cpu_usage, fetch_ram_usage, fetch_disk_usage, fetch_network_usage
 from gevent.pywsgi import WSGIServer
-import requests
 
 app = Flask(__name__)
 
@@ -13,11 +12,27 @@ def add_header(response):
     response.headers['Access-Control-Allow-Methods'] = 'GET'
     return response
 
-@app.route('/server/cpu')
-def server_cpu():
-    return jsonify({'cpu_usage': start_fetching(), 'cpu_limit': '200%'})
+@app.route('/cpu-usage')
+def get_cpu_usage():
+    cpu_usage = fetch_cpu_usage()
+    if cpu_usage is None:
+        return jsonify({'error': 'Failed to retrieve server information'})
+    return jsonify({'cpu_usage': cpu_usage})
+
+@app.route('/ram-usage')
+def get_ram_usage():
+    ram_usage = fetch_ram_usage()
+    if ram_usage is None:
+        return jsonify({'error': 'Failed to retrieve server information'})
+    return jsonify({'ram_usage': ram_usage})
+
+@app.route('/disk-usage')
+def get_disk_usage():
+    disk_usage = fetch_disk_usage()
+    if disk_usage is None:
+        return jsonify({'error': 'Failed to retrieve server information'})
+    return jsonify({'disk_usage': disk_usage})
 
 if __name__ == '__main__':
-    # Run the Flask app using the WSGI server
     http_server = WSGIServer(('0.0.0.0', 5000), app)
     http_server.serve_forever()
